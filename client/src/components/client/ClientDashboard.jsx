ed756eebb8a47c07955f7c5d1669da0cef1932e0
axessmongo <axessmongo@gmail.com>
2024-03-22 11:43:15 +0530

    new changes for sundar

--- client/src/components/client/ClientDashboard.jsx
+++ client/src/components/client/ClientDashboard.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link, NavLink, Outlet, useLocation } from 'react-router-dom'
 
 export default function ClientDashboard() {
-    
+
 
     const location = useLocation();
 
@@ -17,9 +17,9 @@ export default function ClientDashboard() {
         <>
             <div className='dashboard'>
                 <div className="container-fluid">
-                    <div className="row gx-4">
-                        <div className="col-md-2">
-                            <nav className="navbar navbar-expand-lg min-vh-100 align-items-start">
+                    <div className="row gx-4 position-relative">
+                        <div className="col-lg-4 col-xl-3 col-xxl-2 dash-sticky">
+                            <nav className="navbar navbar-expand-xl min-vh-100 align-items-start d-flex flex-column justify-content-between">
                                 <div className="container-fluid">
                                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
                                         <span className="navbar-toggler-icon"></span>
@@ -36,7 +36,7 @@ export default function ClientDashboard() {
                                             </li>
                                             <li className="nav-item">
                                                 <NavLink to={'clientselected'} className="nav-link">
-                                                    <i className="bi bi-bookmark-check"></i>Selected Projects</NavLink>
+                                                    <i className="bi bi-bookmark-check"></i> Intrested Projects</NavLink>
                                             </li>
                                             <li className="nav-item">
                                                 <NavLink to={'clientbids'} className="nav-link">
@@ -46,13 +46,36 @@ export default function ClientDashboard() {
                                         </ul>
                                     </div>
                                 </div>
+                                <div className="container-fluid mb-3">
+                                    <div className='profile myshadow bg-white w-100 rounded-4 py-3 d-flex justify-content-center flex-column align-items-center'>
+                                        <div className='profile-img'>
+                                            <i className="bi bi-person-circle display-4"></i>
+                                        </div>
+
+                                        <div className='profile-info'>
+                                            <p className='lead fs-6 fw-semibold m-0 color2'>Sundar1007</p>
+                                        </div>
+                                        <div className='mt-3'>
+                                            <button className="btn btn-outline-primary fs-6">
+                                                Update Profile
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
                             </nav>
                         </div>
-                        <div className="col-md-10">
+                        <div className="col-lg-8 col-xl-9 col-xxl-10">
                             <div className='p-3'>
                                 <div className='d-flex justify-content-between align-items-center'>
                                     {/* <p className='lead fs-6 fw-normal'>{window.location.pathname.slice(1)}</p> */}
-                                    <p className='lead fs-6 fw-semibold m-0'>Sundar1007</p>
+                                    <div>
+                                        <input type="checkbox" class="checkbox" id="checkbox"/>
+                                            <label for="checkbox" class="checkbox-label">
+                                                <i class="fas fa-moon bi bi-moon-fill"></i>
+                                                <i class="fas fa-sun bi bi-sun-fill"></i>
+                                                <span class="ball"></span>
+                                            </label>
+                                    </div>
                                     <div>
                                         <Link to={'/'} className='btn btn-danger'>Logout</Link>
                                     </div>
