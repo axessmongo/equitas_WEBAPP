75ef11cfbb03b247c8c5e28eaf9b152cb61b593a
axessmongo <axessmongo@gmail.com>
2024-03-29 16:08:39 +0530

    final changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,15 @@
 import MainRouter from './MainRouter';
 
 function App() {
+
+  const arr = [1,2,3,4,5]
+  
+  let [one, two, ...balanced] = arr
+
+  console.log(one, two, balanced);
+
   return (
-    <div className="App">
+    <div clas sName="App">
       <MainRouter/>
     </div>
   );
