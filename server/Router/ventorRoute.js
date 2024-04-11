56134be005ddf81f35b1c584b79963c0a4ba6126
axessmongo <axessmongo@gmail.com>
2024-04-11 19:09:58 +0530

    final commit

--- server/Router/ventorRoute.js
+++ server/Router/ventorRoute.js
@@ -6,7 +6,7 @@ const ventorDetailrouter =require("../module/ventorcontrol.js")
 router.post("/api/register",ventorDetailrouter.RegisterPostMethod);
 router.post("/api/login", ventorDetailrouter.loginMethod);
 // router.put("/api/bookmarkprojects/:id",ventorDetailrouter.Bookmarkprojects);
-router.post("/api/intrestedprojects/:id", ventorDetailrouter.Bookmarkprojects)
+router.post("/api/interestedprojects/:id", ventorDetailrouter.Bookmarkprojects)
 router.post("/api/emailpassword", ventorDetailrouter.Emailpassword);
 router.get("/api/singleuser/:id", ventorDetailrouter.getIdMethod);
 router.post("/api/biddedvalues/:id", ventorDetailrouter.Bidvalue);
